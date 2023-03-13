d779e8915f106391e4a1e4f40964a1338d71fefb
Anas-Altaf <anashafiz926@gmail.com>
2023-03-13 15:28:11 +0500

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,8 +1,8 @@
 import openai
 
 # OpenAI-API (get on OpenAI - must be of an USA account)
-openai.api_key = 'HIDDEN'
+openai.api_key = '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'
 
 # Token for fanpage and verify it
-_PAGE_ACCESS_TOKEN = 'HIDDEN'
-_VERIFY_TOKEN = 'HIDDEN'
+_PAGE_ACCESS_TOKEN = 'EAACK8FNM3GYBAIqBuCdeDNzcB5PhAdsgRect2oKZBCRdYaakxqyxoKFkJZAtRTFnl4VZCZBX6O42XNsh9PZCJAuXQzzhk4hmSrUJtJwghpOz4QZCsWhZCy8ZAZCV7hZATdbwM0KAdrmN17CM5wcZBexLyouX62RPwaYR3IRQDN0GbpZB5GgYm82weGLJ'
+_VERIFY_TOKEN = 'xit'
